050968df1bcb904306b98ad0f84a8f532de4ba39
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-18 14:24:17 -0600

    not sure why testing is breaking now with color and colorList test

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
+import { handlers } from '../mocks/handlers';
 
-const testColor = '#555';
+
+const testColor = {handlers};
 
 test("Renders without errors with blank color passed into component", () => {
     render(<Color hex={testColor}/>);
