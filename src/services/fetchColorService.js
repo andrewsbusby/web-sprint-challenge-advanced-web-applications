e95ea37a798a27b8e7b35cb21689afb13b266ab3
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-18 17:15:38 -0600

    trying to get login to work

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -4,13 +4,15 @@ import {axiosAuth} from '../helpers/axiosWithAuth';
 const fetchColorService = () => {
     
     axiosAuth()
-        .get('/colors')
+        .get('../components/Color.js')
         .then(res => {
             console.log(res.data);
         })
         .catch(err => {
             console.log(err);
         })
+        
+       
 }
 
 export default fetchColorService;
