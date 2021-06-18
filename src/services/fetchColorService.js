dd99f0f7be5d9dcfbe460bef5545407caa0c12a1
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-18 13:35:46 -0600

    built fetchcolorservice with axioswithauth

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,14 @@
 import axiosWithAuth from '../helpers/axiosWithAuth';
 
 const fetchColorService = () => {
-    
+    axiosWithAuth()
+        .get('/colors')
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 export default fetchColorService;
