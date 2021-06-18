889c65dbb15c251303b13d347179e092a28b80f2
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-18 14:44:50 -0600

    trying to get tests to pass

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
 
+
+const testColor = {
+    orange: '#d45d0c',
+    blue: '#495f85',
+    red: '#af1f1f',
+
+}
 test("Renders without errors", ()=> {
     render(<BubblePage />)
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
-    render(<BubblePage/>);
+    render(<BubblePage color={testColor}/>);
 
     const bubbles = screen.getByText(/color/i);
     expect(bubbles).toBeInTheDocument();
