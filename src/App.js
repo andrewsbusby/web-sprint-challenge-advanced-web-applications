cd4303c6e51e8b9460256766cc2bdb4def01bcef
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-18 13:32:37 -0600

    setup privateroute in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import BubblePage from './components/BubblePage';
@@ -12,10 +12,11 @@ function App() {
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a data-testid="logoutButton" href="/">logout</a>
         </header> 
 
         <Route exact path="/" component={Login} />
+        <PrivateRoute path='/bubblepage' ocmponent={BubblePage}/>
       </div>
     </Router>
   );
