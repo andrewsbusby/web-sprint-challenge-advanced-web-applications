f4d7c7a77d2506273aae1cab6337c03c46b87915
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-18 14:33:02 -0600

    fixing in app.js so it will render

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
 import BubblePage from './components/BubblePage';
-
+import PrivateRoute from './components/PrivateRoute'
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+const logout = localStorage.removeItem('token')
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="/">logout</a>
+          <a data-testid="logoutButton" href="/" onClick ={logout}>logout</a>
         </header> 
 
-        <Route exact path="/" component={Login} />
-        <PrivateRoute path='/bubblepage' ocmponent={BubblePage}/>
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path= '/bubblepage' component={BubblePage}/>
+
       </div>
     </Router>
   );
